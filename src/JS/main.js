c6467b4295753914ef696052e473a0095acf88df
davidgomezfrieiro <davidgomezfrieiro@gmail.com>
2022-05-04 09:56:44 +0200

    loader

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,17 +1,17 @@
-"use strict";
-import { currentDate } from "./helpers.js";
+'use strict';
+import { currentDate } from './helpers.js';
 
 // Función asíncrona para solicitar el token de acceso para la API
 async function fetchToken() {
   try {
     const responseToken = await fetch(
-      "https://test.api.amadeus.com/v1/security/oauth2/token",
+      'https://test.api.amadeus.com/v1/security/oauth2/token',
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: "grant_type=client_credentials&client_id=DzFz6xgx71Nr8B1hOi2BUjZHxVFGCcb4&client_secret=n0fSOjZX4L1PNBKq",
+        body: 'grant_type=client_credentials&client_id=DzFz6xgx71Nr8B1hOi2BUjZHxVFGCcb4&client_secret=n0fSOjZX4L1PNBKq',
       }
     );
     const dataToken = await responseToken.json();
@@ -23,17 +23,28 @@ async function fetchToken() {
 }
 fetchToken();
 
-const form = document.querySelector("form");
-form.addEventListener("submit", async (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   let locations = {
-    origin: document.querySelector("#origin").value.toUpperCase(),
-    destination: document.querySelector("#destination").value.toUpperCase(),
+    origin: document.querySelector('#origin').value.toUpperCase(),
+    destination: document.querySelector('#destination').value.toUpperCase(),
   };
+  const gallery = document.querySelector('#flightGallery');
+  const loader = document.createElement('div');
+  loader.classList.add('lds-ellipsis');
+  loader.innerHTML = '<div></div><div></div><div></div><div></div>';
+  gallery.appendChild(loader);
+  const ul = document.querySelector('#resultList');
+  if (ul.firstChild) {
+    ul.firstChild.remove();
+  }
   const cheapestFlight = await fetchApi(locations);
-  const ul = document.querySelector("#resultList");
-  const li = document.createElement("li");
-  li.innerHTML = `<article><p></p></article>`;
+
+  const li = document.createElement('li');
+  li.classList.add('flightArticle');
+  li.innerHTML = `<article><p>a</p></article>`;
+  loader.remove();
   ul.appendChild(li);
 });
 
